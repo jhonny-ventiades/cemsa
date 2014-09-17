aa57772dbc3cdfda69effca1122f537630d1df89
Jose Enrique <jose.iglesias@gbsnegocios.com>
2014-09-17 14:58:48 -0400

    Implemented method for save a new user in the database and also the method for authenticate an user in the system

--- servidor/controllers/usuario.js
+++ servidor/controllers/usuario.js
@@ -1,14 +1,17 @@
 var db = require('../config/env/config_db');
+var btoa = require('btoa');
 
 /**
- *Function for save a new User
+ *Funcion para registrar un nuevo usuario en la base de datos
  **/
-exports.saveUser= function (req, res){
+exports.crearUsuario= function (req, res){
 
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
 
+    var passwordEncriptado = btoa(req.body.passwordUsuario);
+    console.log(passwordEncriptado);
     db.Usuario
-        .build({ name: req.body.name, rol: req.body.rol, status: true})
+        .build({ loginUsuario: req.body.loginUsuario, passwordUsuario:passwordEncriptado, estadoUsuario:true, funcionarioId:req.body.idFuncionario})
         .save()
         .success(function() {
             return res.json(200)
@@ -18,47 +21,29 @@ exports.saveUser= function (req, res){
 };
 
 /**
- *  Function for show all endpoints
+ *  Funcion para verificar y autenticar un usuario en el sistema
  */
-exports.findAllUsers=function(req, res, next){
+exports.verificarUsuario = function(req, res){
 
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
 
+    var passwordEncriptado = btoa(req.params.password);
+    var accesoUsuario = {};
     db.Usuario
-        .all()// Here is the method which take all the information in the table
-        .complete(function(err, user) {
-            if (err) {
-                return next(err);
+        .find({where:{ loginUsuario: req.params.user, passwordUsuario:passwordEncriptado },include:[{model: db.Funcionario, required:true}]})
+        .complete(function(err, usuario){
+            if(err){
+                return res.json(400,err);
             }
             else
-            if(!user){
-                return res.json(404);
+            if(!usuario){
+                return res.json(404,{error:"Usuario no encontrado"});
             }
-            else {
-                // We return all the endpoints that exists in the table in a JSON format
-                return res.json(user)
+            else{
+                accesoUsuario = {idUsuario: usuario.idUsuario, loginUsuario: usuario.loginUsuario, estadoUsuario:usuario.estadoUsuario,
+                    idFuncionario: usuario.funcionario.idFuncionario, nombre: usuario.funcionario.nombre, apellidoPaterno: usuario.funcionario.apellidoPaterno,
+                    apellidoMaterno:usuario.funcionario.apellidoMaterno, estadoFuncionario:usuario.funcionario.estadoFuncionario, cargo: usuario.funcionario.cargo};
+                return res.json(accesoUsuario);
             }
-        });
-};
-
-/**
- *Funtion that allow us get a specific User by ID
- */
-exports.findUserById=function(req, res, next){
-
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-
-    var idUser=req.params.id;//Here we receive the id_user of the user that we can get information
-    db.Usuario
-        .find({ where: { idUsuario: idUser } })//Here we check if the user exists in the database
-        .complete(function(err, user) {
-            if (err) {
-                return next(err);
-            } else if (!user) {
-                return res.json(404);// If the user with the id_user that we have receive doesn't exist we can see here
-            } else {
-                // Here we return the information of the user which the id received.
-                return res.json(user)
-            }
-        });
+        })
 };
